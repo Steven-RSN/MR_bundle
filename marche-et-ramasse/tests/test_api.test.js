9a75a96200e9a1d54ccf6a8bf81dd375f093fe13
Steven-RSN <stevenraison@outlook.fr>
2026-03-30 11:53:24 +0200

    comentary

--- marche-et-ramasse/tests/test_api.test.js
+++ marche-et-ramasse/tests/test_api.test.js
@@ -32,27 +32,27 @@ const API_URL = 'https://192.168.1.63:5173/api'; //backend
 
 describe("sendDechetToServer()", () => {
     test("Retourne la réponse du serveur lors de l'envoi de la requête pour enregistrer un déchet ", async () => {
-
+      
       // represente le payload attendu par registerDechet()
-        const dechet_test = {
-            idUser: 1,
-            images: "uploads/1/img_0.webp",
-            type_dechet: "Plastique",
-            volume_litres: 1.5,
-            poids_kg: 0.5,
-            lieu: "Toulouse",
-            commentaire: "Un tas de bouteilles en plastique trouvé au milieu du trottoir.",
-            latitude: 43.599998,
-            longitude: 1.43333,
-            date: new Date().toISOString()
-        }
+      const dechet_test = {
+        idUser: 1,
+        images: "uploads/1/img_0.webp",
+        type_dechet: "Plastique",
+        volume_litres: 1.5,
+        poids_kg: 0.5,
+        lieu: "Toulouse",
+        commentaire: "Un tas de bouteilles en plastique trouvé au milieu du trottoir.",
+        latitude: 43.599998,
+        longitude: 1.43333,
+        date: new Date().toISOString()
+      }
 
       //la fonction appelle sendDechetToServer() puis renvoi response.data
-        const result = await sendDechetToServer(dechet_test);
+      const result = await sendDechetToServer(dechet_test);
 
       // Verification de la donnée attendue vs reçue: 
       // Attendu : retourne { success: true }
-        expect(result.success).toEqual(true);
+      expect(result.success).toEqual(true);
     });
 
     test('')
